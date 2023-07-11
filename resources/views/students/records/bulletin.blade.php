7fcb4fecf4c806fec3e35b1a603dbcbeb4e2cd9a
Joselopeznachar28 <joselopeznachar28@gmail.com>
2023-07-11 08:21:39 -0400

    continue bulletin

--- resources/views/students/records/bulletin.blade.php
+++ resources/views/students/records/bulletin.blade.php
@@ -7,11 +7,44 @@
     <title>Boletin</title>
 </head>
 <body>
-    <label for="">Materias</label>
-    @foreach ($student_record->year_school->subjects as $k => $subject)
+
+        <div style="width: 50%;">
+            <!-- primer momento pedagogico -->
+            <span>I MOMENTO PEDAGOGICO</span><br>
+            <span>Observaciones / Recomendaciones</span><br>
+            <span>____________________________________________</span><br>
+            <span>____________________________________________</span><br>
+            <span>G.C.R.P ( )</span> <span>O.C ( )</span> <span>C.E ( )</span><br><br><br>
+            <span style="border-top: 2px solid #000000; margin-right: 2rem;">Profesor(a) Guia</span> <span style="border-top: 2px solid #000000; margin-right: 2rem;">Representante</span><span style="border-top: 2px solid #000000; margin-right: 2rem;">Fecha</span><br><br>
+            <!-- segundo momento pedagogico -->
+            <span>II MOMENTO PEDAGOGICO</span><br>
+            <span>Observaciones / Recomendaciones</span><br>
+            <span>____________________________________________</span><br>
+            <span>____________________________________________</span><br>
+            <span>G.C.R.P ( )</span> <span>O.C ( )</span> <span>C.E ( )</span><br><br><br>
+            <span style="border-top: 2px solid #000000; margin-right: 2rem;">Profesor(a) Guia</span> <span style="border-top: 2px solid #000000; margin-right: 2rem;">Representante</span><span style="border-top: 2px solid #000000; margin-right: 2rem;">Fecha</span><br><br>
+            <!-- tercer momento pedagogico -->
+            <span>III MOMENTO PEDAGOGICO</span><br>
+            <span>Observaciones / Recomendaciones</span><br>
+            <span>____________________________________________</span><br>
+            <span>____________________________________________</span><br>
+            <span>G.C.R.P ( )</span> <span>O.C ( )</span> <span>C.E ( )</span><br><br><br>
+            <span style="border-top: 2px solid #000000; margin-right: 2rem;">Profesor(a) Guia</span> <span style="border-top: 2px solid #000000; margin-right: 2rem;">Representante</span><span style="border-top: 2px solid #000000; margin-right: 2rem;">Fecha</span>
+            <h4 style="text-align: center;">EVALUACION CUALITATIVA</h4>
+            <p>A - Evidencia un excelente desarrollo de sus potencialidades, tomando en cuenta su participacion individual y colectiva, durante los procesos de aprendizaje.</p>
+            <p>B - Evidencia un buen desarrollo de sus potencialidades, tomando en cuenta su participacion individual y colectiva, durante los procesos de aprendizaje. Debe continuar fortaleciendo los procesos de aprendizaje.</p>
+            <p>C - Evidencia un satisfactorio desarrollo de sus potencialidades, tomando en cuenta su participacion individual y colectiva, durante los procesos de aprendizaje. Requiere reorientacion para la consolidacion de los procesos de aprendizaje.</p>
+            <p>D - Evidencia un aceptable desarrollo de sus potencialidades, tomando en cuenta su participacion individual y colectiva, durante los procesos de aprendizaje. Requiere reforzar y reorientar para la consolidacion de los procesos de aprendizaje.</p>
+        </div>
+        <div style="width: 50%; float: right;">
+            
+        </div>
+    {{-- @foreach ($student_record->year_school->subjects as $k => $subject)
             <div class="row">
-                <span>{{ $subject->name }}</span>
+                <div class="col-sm-12">
+                    <span>{{ $subject->name }}</span>
+                </div>
             </div>
-    @endforeach
+    @endforeach --}}
 </body>
 </html>
